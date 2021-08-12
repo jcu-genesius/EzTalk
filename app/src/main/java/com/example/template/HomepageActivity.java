7731b2c4c84b02c7bfe6cca4610565bea99ea318
justina823 <laisumjustina.chan@my.jcu.edu.au>
2021-08-12 22:11:34 +1000

    add contact

--- app/src/main/java/com/example/template/HomepageActivity.java
+++ app/src/main/java/com/example/template/HomepageActivity.java
@@ -2,13 +2,19 @@ package com.example.template;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomepageActivity extends AppCompatActivity {
+    private Button Contact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
     }
+
 }
